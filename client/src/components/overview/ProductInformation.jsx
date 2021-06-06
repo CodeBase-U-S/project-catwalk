698318e6b22c2856ee5abc4b799dd07ff8aeceb6
Jeremy Lee <jeremysylee@gmail.com>
2021-06-05 16:21:36 -1000

    Created overview folder containing Overview subcomponent jsx files. Overview component skeleton complete. All components render rudimentary div blocks.

---
+++ client/src/components/overview/ProductInformation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ProductInformation = (props) => {
+
+  return (
+    <div id="product-info">
+      <div>Star Rating Placeholder</div>
+      <div>{props.data.category}</div>
+      <div>{props.data.name}</div>
+      <div>${props.data.default_price}</div>
+      <div>{props.data.description}</div>
+    </div>
+  )
+};
+
+export default ProductInformation;
