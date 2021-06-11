dd4982a99037f93e98dcf3ede68d2543d13c2ff8
Miguel Buenviaje <migbuen44@gmail.com>
2021-06-10 22:14:58 -0700

    Implemented functional search bar to query questions

--- client/src/components/QAcomponents/search.jsx
+++ client/src/components/QAcomponents/search.jsx
@@ -1,22 +1,38 @@
 import React, {useState} from 'react';
 
-let Search = () => {
-
-  let [value, setValue] = useState('Have A Question? Search For Answers...')
+let Search = ({questions, setQuestions, searchQuestions}) => {
+  console.log('searchQuestions in search: ', searchQuestions);
+  let [value, setValue] = useState('');
 
   let handleSearchChange = (e) => {
-    // console.log(e.target.value)
+    // console.log('questions:', questions);
     let currentValue = e.target.value;
-    return setValue(currentValue)
-  }
+    // setQuestions([questions[0]]);
+    // console.log('value length: ', currentValue.length);
+    if (currentValue.length >= 3) {
+      let matchedQuestions = [];
+      searchQuestions.forEach(question => {
+        if (question.question_body.includes(currentValue)) {
+          matchedQuestions.push(question);
+        }
+      });
+
+      setQuestions(matchedQuestions);
+    } else {
+      console.log('questions in else: ', searchQuestions);
+      setQuestions(searchQuestions);
+    }
+
+    return setValue(currentValue);
+  };
 
 
   return (
     <form>
       <input type='text' value={value} onChange={handleSearchChange}/>
     </form>
-  )
-}
+  );
+};
 
 
 
--- client/src/components/QandA.jsx
+++ client/src/components/QandA.jsx
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react';
-import Search from './QAcomponents/search.jsx'
-import Question from './QAcomponents/question.jsx'
-import axios from 'axios'
-import _ from 'underscore'
+import Search from './QAcomponents/search.jsx';
+import Question from './QAcomponents/question.jsx';
+import axios from 'axios';
+import _ from 'underscore';
 
 const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax';
 
@@ -10,61 +10,66 @@ const auth = {
   headers: {
     Authorization: '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'
   }
-}
+};
 
 let QandA = () => {
 
   //function to get info from atelier api
-  let [questions, setQuestions] = useState([])
-  let [questionCount, setQuestionCount] = useState(2)
+  let [questions, setQuestions] = useState([]);
+  let [questionCount, setQuestionCount] = useState(2);
+  let [searchQuestions, setSearchQuestions] = useState([]);
+
 
   let retrieveQuestions = (page, count) => {
     axios.get(`${url}/qa/questions?product_id=16056&page=${page}&count=${count}`, auth)//refactor for product id input
       .then(({data}) => {
-        console.log('questions response:', data.results)
+        console.log('questions response:', data.results);
         //perform sort here
         let sortedQuestions = _.sortBy(data.results, (question) => {
           return question.question_helpfulness;
-        })
+        });
         // console.log('sortedQuestions: ', sortedQuestions.reverse())
-        setQuestions(sortedQuestions.reverse())
+        setQuestions(sortedQuestions.reverse());
+        setSearchQuestions(sortedQuestions.reverse());
+        // console.log('searchQuestions:', searchQuestions);
       })
       .catch(err => {
-        console.log(err)
-      })
-  }
+        console.log(err);
+      });
+  };
 
 
   // retrieveQuestions();
   useEffect(() => {
-    retrieveQuestions(1, questionCount)
-  }, [])
+    retrieveQuestions(1, questionCount);
+  }, []);
 
   let moreQuestionsHandler = () => {
     setQuestionCount(prevQuestionCount => {
-      let currentCount = prevQuestionCount + 2
-      retrieveQuestions(1, currentCount)
-      return currentCount
-    })
+      let currentCount = prevQuestionCount + 2;
+      retrieveQuestions(1, currentCount);
+      return currentCount;
+    });
     // retrieveQuestions(1, questionCount)
-  }
-
+  };
+  console.log('searchQuestions before return: ', searchQuestions);
   return (
     <>
-      <Search />
+      <Search setQuestions={setQuestions} questions={questions}
+        searchQuestions={searchQuestions}/>
       <div>
         {questions.map(question => {
           // return <Question question={question.question_body} helpfulCount={question.question_helpfulness}/>//<>Questions...Answers</>
-          return <Question question={question}/>
+          return <Question question={question}/>;
         })}
       </div>
       <div onClick={moreQuestionsHandler}>
         More Questions
       </div>
     </>
-  )
+  );
 
-}
+};
 
 
 export default QandA;
