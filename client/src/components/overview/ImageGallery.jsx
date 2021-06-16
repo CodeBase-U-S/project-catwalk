26ab02bf6ee9f1a10845a2695c0a0a379cded1eb
Jeremy Lee <jeremysylee@gmail.com>
2021-06-15 21:35:40 -1000

    added rudimentary vertical navigation buttons. Non-functional. Moved the buttons outside of the overflow so they do not scroll with the thumbnail gallery. Chevrons are not vertically centered.

--- client/src/components/overview/ImageGallery.jsx
+++ client/src/components/overview/ImageGallery.jsx
@@ -49,8 +49,18 @@ const ImageGallery = (props) => {
       <Jumbotron>
         <Container>
           <Row>
-            <Col xs={2} id="thumbnailGallery" style={{height: '75vh', overflowY: 'scroll'}}>
-              <div>
+            <Col xs={2} id="thumbnailGallery" style={{height: '75vh'}}>
+              <button id='navigation-vert' className='tg-top' onClick={navLeft}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" className="bi bi-chevron-compact tg-up" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894l6-3z"/>
+                </svg>
+              </button>
+              <button id='navigation-vert' className='tg-bottom' onClick={navLeft}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" class="bi bi-chevron-compact tg-down" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z"/>
+                </svg>
+              </button>
+              <div id="thumbnailGallery" style={{height: '75vh', overflowY: 'scroll'}}>
                 {selectedStyle && selectedStyle.photos.map((photo, id) => (
                   <div id='thumbnail' key={id} className="mb-4" >
                     {photoIndex === id ?
@@ -64,14 +74,14 @@ const ImageGallery = (props) => {
             </Col>
             <Col xs={10} style={{justifyContent: 'center'}}>
               {photoIndex > 0 &&
-            <button id='navigation' className='left' onClick={navLeft}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" className="bi bi-chevron-compact left" viewBox="0 0 16 16">
+            <button id='navigation' className='tg-left' onClick={navLeft}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" className="bi bi-chevron-compact tg-left" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z"/>
               </svg>
             </button>}
               {photoIndex < selectedStyle.photos.length - 1 &&
-            <button id='navigation' className='right' onClick={navRight}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" className="bi bi-chevron-compact right" viewBox="0 0 16 16">
+            <button id='navigation' className='tg-right' onClick={navRight}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" className="bi bi-chevron-compact tg-right" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"/>
               </svg>
             </button>}
