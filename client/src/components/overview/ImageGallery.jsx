c8e7e37923d86203e9047cab0f451ebccfe1cf90
Jeremy Lee <jeremysylee@gmail.com>
2021-06-16 08:54:30 -1000

    imerge conflicts resolved

--- client/src/components/overview/ImageGallery.jsx
+++ client/src/components/overview/ImageGallery.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Row, Col, Image, Jumbotron, Container } from 'react-bootstrap';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation, Controller } from 'swiper/core';
 
 const ImageGallery = (props) => {
   const dispatch = useDispatch();
@@ -18,18 +16,32 @@ const ImageGallery = (props) => {
     if (photoIndex < selectedStyle.photos.length - 1) {
       dispatch({ type: 'SET_PHOTO', photoIndex: photoIndex + 1});
     }
+    scrollTo(scrollPositionFinderRight);
   };
 
   const navLeft = () => {
     if (photoIndex > 0) {
       dispatch({ type: 'SET_PHOTO', photoIndex: photoIndex - 1});
     }
+    scrollTo(scrollPositionFinderLeft);
   };
 
-  SwiperCore.use([Navigation]);
-  SwiperCore.use([Controller]);
-  const [firstSwiper, setFirstSwiper] = useState(null);
-  const [secondSwiper, setSecondSwiper] = useState(null);
+  const scrollTo = (scrollPositionFinder) => {
+    let scrollContainer = document.getElementById('thumbnailGallery');
+    scrollContainer.scrollTo({top: scrollPositionFinder(), behavior: 'smooth'});
+  };
+
+  const scrollPositionFinderRight = () => {
+    let viewportHeight = window.innerHeight;
+    let avgHeight = viewportHeight / selectedStyle.photos.length;
+    return (avgHeight * (photoIndex + 1));
+  };
+
+  const scrollPositionFinderLeft = () => {
+    let viewportHeight = window.innerHeight;
+    let avgHeight = viewportHeight / selectedStyle.photos.length;
+    return (avgHeight * (photoIndex - 1));
+  };
 
 
   if (selectedStyle) {
@@ -50,11 +62,11 @@ const ImageGallery = (props) => {
               </button>
               <div id="thumbnailGallery" style={{height: '75vh', overflowY: 'scroll'}}>
                 {selectedStyle && selectedStyle.photos.map((photo, id) => (
-                  <div id='thumbnail' key={id} className="mb-5" >
+                  <div id='thumbnail' key={id} className="mb-4" >
                     {photoIndex === id ?
-                      <Image src={photo.thumbnail_url} thumbnail key={id} value={photo} onClick={() => selectPhoto(photo, id)} style={{opacity: '60%'}}/>
+                      <Image id={`pIndex${id}`} src={photo.thumbnail_url} thumbnail key={id} value={photo} onClick={() => selectPhoto(photo, id)} style={{opacity: '60%'}}/>
                       :
-                      <Image src={photo.thumbnail_url} thumbnail key={id} value={photo} onClick={() => selectPhoto(photo, id)} />
+                      <Image id={`pIndex${id}`} src={photo.thumbnail_url} thumbnail key={id} value={photo} onClick={() => selectPhoto(photo, id)} />
                     }
                   </div>
                 ))}
@@ -62,14 +74,14 @@ const ImageGallery = (props) => {
             </Col>
             <Col xs={10} style={{justifyContent: 'center'}}>
               {photoIndex > 0 &&
-            <button id='navigation' className='left' onClick={navLeft}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" className="bi bi-chevron-compact left" viewBox="0 0 16 16">
+            <button id='navigation' className='tg-left' onClick={navLeft}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" className="bi bi-chevron-compact tg-left" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z"/>
               </svg>
             </button>}
               {photoIndex < selectedStyle.photos.length - 1 &&
-            <button id='navigation' className='right' onClick={navRight}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" className="bi bi-chevron-compact right" viewBox="0 0 16 16">
+            <button id='navigation' className='tg-right' onClick={navRight}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" className="bi bi-chevron-compact tg-right" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"/>
               </svg>
             </button>}
@@ -77,25 +89,6 @@ const ImageGallery = (props) => {
                 <Image src={selectedStyle.photos[photoIndex].url} style={{height: '75vh', objectFit: 'cover', width: '102%'}}/>}
             </Col>
           </Row>
-{/*
-        <div>
-          <Swiper
-            spaceBetween={50}
-            slidesPerView={5}
-            navigation={true}
-            onSlideChange={() => console.log('slide changed')}
-            onSwiper={(swiper) => console.log(swiper)}>
-            {selectedStyle && selectedStyle.photos.map((photo, id) => (
-              <SwiperSlide key={id}>
-                {photoIndex === photo ?
-                  <Image className="carouselThumbnail" thumbnail src={photo.thumbnail_url} key={id} value={photo} onClick={() => selectPhoto(photo, id)} style={{opacity: '60%'}}/>
-                  :
-                  <Image className="carouselThumbnail" thumbnail src={photo.thumbnail_url} key={id} value={photo} onClick={() => selectPhoto(photo, id)} />
-                }
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div> */}
         </Container>
       </Jumbotron>
     );
@@ -105,4 +98,3 @@ const ImageGallery = (props) => {
 };
 
 export default ImageGallery;
-
